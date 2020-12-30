6897c98d51a57cbc12b5035639fb0e7e5c815a43
Paul Drury <paul@drurys.org>
2020-12-30 11:47:44 +0000

    Release Candidate 0.9.1

--- src/tinymce-comments.js
+++ src/tinymce-comments.js
@@ -3,8 +3,12 @@ tinymce.init({
     paste_as_text: true,
     remove_trailing_brs: true,
     menubar: false,
-    statusbar: false,
+    statusbar: true,
+    elementpath: false,
+    branding: true,
+    contextmenu: 'cut copy paste | bold italic strikethrough | link',
+    contextmenu_never_use_native: true,
     plugins: 'link paste',
-    toolbar: 'bold italic strikethrough | link unlink',
-    valid_elements: '-s/span,-span[style=text-decoration: line-through;],-div,-p,-strong/b,-em/i,-a[href|title|target]',
+    toolbar: 'cut copy paste | bold italic strikethrough | link',
+    valid_elements: 's/span,span[style=text-decoration: line-through;],div,p,strong/b,em/i,a[href|title|target]'
 });
